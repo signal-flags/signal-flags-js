96920247134f2315fe31112149f0e1dfc9a601c3
Paul OpenSums <paul@opensums.com>
2020-11-02 14:08:12 +0000

    chore: fix linting

--- bin/parse-yaml.js
+++ bin/parse-yaml.js
@@ -4,7 +4,7 @@ const { readFile, writeFile } = require('fs').promises;
 const { resolve } = require('path');
 
 const yaml = require('js-yaml');
-const { format } = require("prettier");
+const { format } = require('prettier');
 const prettierOptions = require('../.prettierrc');
 
 const outPath = 'src';
@@ -18,13 +18,14 @@ async function buildFile(path, outPath) {
   // Convert from yaml into pretty JSON.
   parsed = JSON.stringify(yaml.safeLoad(parsed), null, 2);
   // Add a header to turn it into JS.
-  const outFileName = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.')) + '.js';
+  const outFileName =
+    path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.')) + '.js';
   parsed = `// ${outFileName}\nexport default ${parsed};\n`;
   // Make it even prettier.
   parsed = format(parsed, prettierOptions);
   // Write it out.
   await writeFile(resolve(__dirname, myPath, outPath, outFileName), parsed);
-  return { file: outFileName, size: parsed.length }
+  return { file: outFileName, size: parsed.length };
 }
 
 // Need to set this to avoid deprecation warning when parsing from a string.
@@ -33,12 +34,15 @@ const promises = [];
 
 files.forEach((file) => {
   promises.push(
-    buildFile(file, outPath).catch(err => {
-      console.log('Error building', file, err);
-      throw err;
-    }).then(res => {
-      console.log('Built', file, res);
-    })); 
+    buildFile(file, outPath)
+      .catch((err) => {
+        console.log('Error building', file, err);
+        throw err;
+      })
+      .then((res) => {
+        console.log('Built', file, res);
+      })
+  );
 });
 
 Promise.all(promises);
