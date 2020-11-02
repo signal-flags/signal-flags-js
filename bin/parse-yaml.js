d06a1f19bef08317b5d7c45347ab2277e3b89794
Paul OpenSums <paul@opensums.com>
2020-11-02 12:33:25 +0000

    chore: refactor yaml parsing

--- bin/parse-yaml.js
+++ bin/parse-yaml.js
@@ -1,24 +1,41 @@
 // bin/build-flags.js
 
-const fs = require('fs');
+const { readFile, writeFile } = require('fs').promises;
 const { resolve } = require('path');
 
 const yaml = require('js-yaml');
+const { format } = require("prettier");
 
-const files = ['default.flags.yaml'];
+const outPath = '../src';
 
-// Get document, or throw exception on error
-try {
-  const inPath = resolve(__dirname, '..', 'designs');
-  const outPath = resolve(__dirname, '..', 'src');
-  files.forEach((file) => {
-    const inFile = fs.readFileSync(resolve(inPath, file), 'utf8');
-    const json = JSON.stringify(yaml.safeLoad(inFile), null, 2);
-    const outFileName = file.split('.');
-    const outFile = `// ${outFileName}\nexport default ${json};\n`;
-    outFileName[outFileName.length - 1] = 'js';
-    fs.writeFileSync(resolve(outPath, outFileName.join('.')), outFile);
-  });
-} catch (e) {
-  console.log(e);
+const files = ['../designs/default.flags.yaml'];
+
+// Build one file from YAML into JSON.
+async function buildFile(path, outPath) {
+  // Read the file in.
+  let parsed = await readFile(resolve(__dirname, path), 'utf8');
+  // Convert from yaml into pretty JSON.
+  parsed = JSON.stringify(yaml.safeLoad(parsed), null, 2);
+  // Add a header to turn it into JS.
+  const outFileName = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.')) + '.js';
+  parsed = `// ${outFileName}\nexport default ${parsed};\n`;
+  // Make it even prettier.
+  parsed = format(parsed);
+  // Write it out.
+  await writeFile(resolve(__dirname, outPath, outFileName), parsed);
+  return { file: outFileName, size: parsed.length }
 }
+
+const promises = [];
+
+files.forEach((file) => {
+  promises.push(
+    buildFile(file, outPath).catch(err => {
+      console.log('Error building', file, err);
+      throw err;
+    }).then(res => {
+      console.log('Built', file, res);
+    })); 
+});
+
+Promise.all(promises);
